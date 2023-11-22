ce32202d898362bed0d1d4cc3d045a0ad3c44dc4
Swayamgaba99 <85446480+Swayamgaba99@users.noreply.github.com>
2023-11-22 22:27:10 +0530

    Add files via upload

---
+++ app/MyComponent/todo/todo.component.ts
@@ -0,0 +1,42 @@
+import { Component,OnInit } from '@angular/core';
+import { Todo } from '../../todos';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+
+export class TodoComponent implements OnInit{
+            localItem: string | null = localStorage.getItem("todos")
+            todos:Todo[];
+            constructor(){ 
+              if(this.localItem==null){
+                this.todos=[];
+              }
+              else{
+                this.todos=JSON.parse(this.localItem);
+              }
+            }
+
+            ngOnInit():void{
+
+            }
+            deleteTodo(todo:Todo){
+              console.log(todo);
+              const index=this.todos.indexOf(todo);
+              this.todos.splice(index, 1);
+              localStorage.setItem("todos",JSON.stringify(this.todos));
+            }
+            addTodo(todo:Todo){
+              console.log(todo);
+              this.todos.push(todo);
+              localStorage.setItem("todos",JSON.stringify(this.todos));
+            }
+            toggleTodo(todo:Todo){
+              const index=this.todos.indexOf(todo);
+              this.todos[index].active=!this.todos[index].active;
+              localStorage.setItem("todos",JSON.stringify(this.todos));
+            }
+
+}
